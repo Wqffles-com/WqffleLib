c9569f2b780b1409032f9c7abf3cfae9b049d158
Wqffles-com <dutchslav@dutchprod.nl>
2024-11-21 20:22:14 +0100

    chore: forgot to add ByteUtilities.cs TwT

---
+++ Tests/ByteUtilities.cs
@@ -0,0 +1,30 @@
+﻿using Utility;
+
+namespace Tests;
+
+public class ByteUtilities
+{
+    [Test(ExpectedResult = new byte[] {1, 2, 3, 4, 5})]
+    public byte[] TrimEnd()
+    {
+        var array = new byte[] { 1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        
+        return array.TrimEnd();
+    }
+
+    [Test(ExpectedResult = new byte[] { 1, 2, 3, 4, 5 })]
+    public byte[] TrimStart()
+    {
+        var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5 };
+
+        return array.TrimStart();
+    }
+
+    [Test(ExpectedResult = new byte[] { 1, 2, 3, 4, 5 })]
+    public byte[] Trim()
+    {
+        var array = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        
+        return array.Trim();
+    }
+}
