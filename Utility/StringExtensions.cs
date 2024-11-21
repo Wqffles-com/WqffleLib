fbde8e833f4eefe55e4a75b167098bfc602b456f
Wqffles-com <dutchslav@dutchprod.nl>
2024-11-21 17:17:28 +0100

    feat: Convert string from and to byte[] using string.ReadAsBytes() and byte[].ReadAsString() feat: Trim byte[] using byte[].Trim() .TrimStart or .TrimEnd()

---
+++ Utility/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Utility;
+
+public static class StringExtensions
+{
+    /// <summary>
+    /// Convert the current string to an array of bytes using the specified encoding.
+    /// </summary>
+    /// <param name="str">The string to convert.</param>
+    /// <param name="encoding">The encoding to use. Defaults to <see cref="Encoding.UTF8"/></param>
+    /// <returns>The string, converted to bytes.</returns>
+    public static byte[] ReadAsBytes(this string str, Encoding? encoding = null)
+    {
+        encoding ??= Encoding.UTF8;
+
+        return encoding.GetBytes(str);
+    }
+}
