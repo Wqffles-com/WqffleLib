7b8997975e3e32c84cfd44b04d85249496ae3d9a
Wqffles-com <cody@astrolink.cc>
2024-12-04 19:18:12 +0100

    docs(util): Documented all the utilities.

--- Utility/EnumerableUtility.cs
+++ Utility/EnumerableUtility.cs
@@ -1,19 +1,46 @@
 ﻿namespace Utility;
 
+/// <summary>
+/// Any utility methods for <see cref="IEnumerable{T}"/>.
+/// </summary>
 public static class EnumerableUtility
 {
+    /// <summary>
+    /// Trim the start of the <see cref="IEnumerable{T}"/> based on the instance.
+    /// </summary>
+    /// <param name="enumerable">The enumerable to be trimmed.</param>
+    /// <param name="instance">The instance to trim of.</param>
+    /// <param name="equalityComparer">Comparer of 2 instances of <see cref="T"/></param>
+    /// <typeparam name="T">The type of the instance to trim.</typeparam>
+    /// <returns>An <see cref="IEnumerable{T}"/>, trimmed at the start of any <see cref="instance"/>s</returns>
     public static IEnumerable<T> TrimStart<T>(this IEnumerable<T> enumerable, T? instance = default, IEqualityComparer<T>? equalityComparer = default)
     {
         equalityComparer ??= EqualityComparer<T>.Default;
         return enumerable.SkipWhile(x => equalityComparer.Equals(x, instance));
     }
     
+    /// <summary>
+    /// Trim the end of the <see cref="IEnumerable{T}"/> based on the instance.
+    /// </summary>
+    /// <param name="enumerable">The enumerable to be trimmed.</param>
+    /// <param name="instance">The instance to trim of.</param>
+    /// <param name="equalityComparer">Comparer of 2 instances of <see cref="T"/></param>
+    /// <typeparam name="T">The type of the instance to trim.</typeparam>
+    /// <returns>An <see cref="IEnumerable{T}"/>, trimmed at the end of any <see cref="instance"/>s</returns>
     public static IEnumerable<T> TrimEnd<T>(this IEnumerable<T> enumerable, T? instance = default, IEqualityComparer<T>? equalityComparer = default)
     {
         equalityComparer ??= EqualityComparer<T>.Default;
         return enumerable.Reverse().SkipWhile(x => equalityComparer.Equals(x, instance)).Reverse();
     }
     
+    /// <summary>
+    /// Trim the start and end of the <see cref="IEnumerable{T}"/> based on the instance.
+    /// </summary>
+    /// <param name="enumerable">The enumerable to be trimmed.</param>
+    /// <param name="instance">The instance to trim of.</param>
+    /// <param name="equalityComparer">Comparer of 2 instances of <see cref="T"/></param>
+    /// <typeparam name="T">The type of the instance to trim.</typeparam>
+    /// <returns>An <see cref="IEnumerable{T}"/>, trimmed of any <see cref="instance"/>s</returns>
     public static IEnumerable<T> Trim<T>(this IEnumerable<T> enumerable, T? instance = default, IEqualityComparer<T>? equalityComparer = default) =>
         enumerable.TrimStart(instance, equalityComparer).TrimEnd(instance, equalityComparer);
 }
--- Utility/StringByteUtility.cs
+++ Utility/StringByteUtility.cs
@@ -2,11 +2,26 @@
 
 namespace Utility;
 
+/// <summary>
+/// Utility methods related to byte[] and string.
+/// </summary>
 public static class StringByteUtility
 {
+    /// <summary>
+    /// Convert a string to a byte array.
+    /// </summary>
+    /// <param name="value">The string to convert</param>
+    /// <param name="encoding">The encoding to use. Defaults to <see cref="Encoding.UTF8"/></param>
+    /// <returns>A byte array corresponding to the string.</returns>
     public static byte[] ReadAsBytes(this string value, Encoding? encoding = null) =>
         (encoding ?? Encoding.UTF8).GetBytes(value);
     
+    /// <summary>
+    /// Convert a byte array to a string.
+    /// </summary>
+    /// <param name="value">The byte array to convert</param>
+    /// <param name="encoding">The encoding to use. Defaults to <see cref="Encoding.UTF8"/></param>
+    /// <returns>A string corresponding to the byte array.</returns>
     public static string ReadAsString(this byte[] value, Encoding? encoding = null) =>
         (encoding ?? Encoding.UTF8).GetString(value);
 }
