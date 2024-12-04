7cbc1ec50007591e31d7a80eb1e0c4d2eb17a20b
Wqffles-com <cody@astrolink.cc>
2024-12-04 17:28:59 +0100

    chore: Created project feat(util): Added IEnumerable<T>.Trim(), TrimEnd() and TrimStart() feat(util): Added string.ReadAsBytes() and byte[].ReadAsString()

---
+++ Utility/EnumerableUtility.cs
@@ -0,0 +1,19 @@
+﻿namespace Utility;
+
+public static class EnumerableUtility
+{
+    public static IEnumerable<T> TrimStart<T>(this IEnumerable<T> enumerable, T? instance = default, IEqualityComparer<T>? equalityComparer = default)
+    {
+        equalityComparer ??= EqualityComparer<T>.Default;
+        return enumerable.SkipWhile(x => equalityComparer.Equals(x, instance));
+    }
+    
+    public static IEnumerable<T> TrimEnd<T>(this IEnumerable<T> enumerable, T? instance = default, IEqualityComparer<T>? equalityComparer = default)
+    {
+        equalityComparer ??= EqualityComparer<T>.Default;
+        return enumerable.Reverse().SkipWhile(x => equalityComparer.Equals(x, instance)).Reverse();
+    }
+    
+    public static IEnumerable<T> Trim<T>(this IEnumerable<T> enumerable, T? instance = default, IEqualityComparer<T>? equalityComparer = default) =>
+        enumerable.TrimStart(instance, equalityComparer).TrimEnd(instance, equalityComparer);
+}
---
+++ Utility/StringByteUtility.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace Utility;
+
+public static class StringByteUtility
+{
+    public static byte[] ReadAsBytes(this string value, Encoding? encoding = null) =>
+        (encoding ?? Encoding.UTF8).GetBytes(value);
+    
+    public static string ReadAsString(this byte[] value, Encoding? encoding = null) =>
+        (encoding ?? Encoding.UTF8).GetString(value);
+}
